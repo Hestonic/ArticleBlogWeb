dba6d48550f85faf26722d9a2911d6b4bb42d144
danii <daniilgeroldovich@gmail.com>
2022-09-14 14:28:44 +0300

    Add Login and Register pages

---
+++ src/pages/Login.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="auth_wrap">
+    <form class="login-form">
+      <my-input
+          v-model="loginForm.login"
+          type="text"
+          placeholder="Login"
+      />
+      <my-input
+          v-model="loginForm.password"
+          type="password"
+          placeholder="Password"
+      />
+      <my-button
+          @click="login"
+      >
+        Login
+      </my-button>
+    </form>
+  </div>
+</template>
+
+<script>
+import MyInput from "@/components/ui/MyInput";
+import MyButton from "@/components/ui/MyButton";
+export default {
+  name: "Login",
+  components: {MyButton, MyInput},
+
+  data() {
+    return {
+      loginForm: {
+        login: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login() {
+      alert("login")
+    }
+  }
+}
+</script>
+
+<style scoped>
+* {
+  margin: 10px;
+}
+.auth_wrap {
+  margin: auto;
+  width: 30%;
+}
+</style>
