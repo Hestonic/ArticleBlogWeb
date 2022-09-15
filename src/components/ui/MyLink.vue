f3b369136c9cb887ab9385d61b1761ec1be64a99
danii <daniilgeroldovich@gmail.com>
2022-09-15 15:06:09 +0300

    Add MyLink ui component

---
+++ src/components/ui/MyLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <a><slot></slot></a>
+</template>
+
+<script>
+export default {
+  name: "my-link"
+}
+</script>
+
+<style scoped>
+a {
+  font-size: larger;
+  color: teal;
+}
+
+a:hover {
+  color: #ffcc00;
+}
+</style>
--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,11 +1,13 @@
 import MyButton from "@/components/ui/MyButton";
 import MyInput from "@/components/ui/MyInput";
 import MyIco from "@/components/ui/MyIco";
+import MyLink from "@/components/ui/MyLink";
 
 
 
 export default [
     MyButton,
     MyInput,
-    MyIco
+    MyIco,
+    MyLink
 ]
