fc0f42d62ce476ff3d70427e822d463d7057e7f3
danii <daniilgeroldovich@gmail.com>
2022-09-15 15:31:55 +0300

    Add Register.vue lay out page

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,13 +1,65 @@
 <template>
+  <div class="auth_wrap">
+    <form class="register-form" @submit.prevent="submit">
 
+      <my-input
+          v-model="registerForm.login"
+          type="text"
+          placeholder="Login"
+      />
+      <my-input
+          v-model="registerForm.password"
+          type="password"
+          placeholder="Password"
+      />
+      <my-input
+          v-model="registerForm.repeatPassword"
+          type="password"
+          placeholder="Repeat password"
+      />
+      <div class="buttons">
+        <my-button
+            type="submit"
+        >
+          Register
+        </my-button>
+        <my-link @click="$router.push('/login')">Login</my-link>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
+import MyInput from "@/components/ui/MyInput";
+import MyButton from "@/components/ui/MyButton";
+import MyLink from "@/components/ui/MyLink";
 export default {
-  name: "Register"
+  name: "Register",
+  components: {MyLink, MyButton, MyInput},
+
+  data() {
+    return {
+      registerForm: {
+        login: '',
+        password: '',
+        repeatPassword: ''
+      }
+    }
+  },
+  methods: {
+    submit() {
+      alert("login")
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+* {
+  margin: 10px;
+}
+.auth_wrap {
+  margin: auto;
+  width: 30%;
+}
 </style>
