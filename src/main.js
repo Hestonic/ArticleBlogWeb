ee32b5d60ba4cad204ca3ad23448ba64aacde1ec
danii <daniilgeroldovich@gmail.com>
2022-10-11 19:18:33 +0500

    Add store

--- src/main.js
+++ src/main.js
@@ -4,10 +4,14 @@ import router from "@/router/router";
 import components from '@/components/ui';
 import store from '@/store';
 
+require('@/store/subscriber')
+
 const app = createApp(App)
 
 components.forEach(component => {
     app.component(component.name, component)
 })
 
-app.use(router).use(store).mount('#app');
+store.dispatch('auth/attempt', localStorage.getItem('token')).then(() => {
+    app.use(router).use(store).mount('#app');
+})
