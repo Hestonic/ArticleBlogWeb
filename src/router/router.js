ca3dc32b719138091326ce6601749a33c792fd04
danii <daniilgeroldovich@gmail.com>
2022-10-15 21:48:13 +0500

    Add auth logic in Vuex modules

--- src/router/router.js
+++ src/router/router.js
@@ -3,29 +3,88 @@ import {createRouter, createWebHistory} from "vue-router";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
 import ArticleIdPage from "@/pages/ArticleIdPage";
+import store from '@/store'
+import WriteArticle from "@/pages/WriteArticle";
+import MyArticles from "@/pages/MyArticles";
+import UpdateArticle from "@/pages/UpdateArticle";
 
 const routes = [
     {
         path: '/',
+        name: 'main',
         component: Main
     },
     {
         path: '/login',
-        component: Login
+        component: Login,
+        beforeEnter: (to, from, next) => {
+            if (store.getters['auth/authenticated']) {
+                return next({
+                    name: 'main'
+                })
+            }
+
+            next()
+        }
     },
     {
         path: '/register',
-        component: Register
+        component: Register,
+        beforeEnter: (to, from, next) => {
+            if (store.getters['auth/authenticated']) {
+                return next({
+                    name: 'main'
+                })
+            }
+
+            next()
+        }
     },
     {
         path: '/article/:id',
         component: ArticleIdPage
+    },
+    {
+        path: '/write-article',
+        component: WriteArticle,
+        beforeEnter: (to, from, next) => {
+            if (!store.getters['auth/authenticated']) {
+                return next({
+                    name: 'main'
+                })
+            }
+            next()
+        }
+    },
+    {
+        path: '/my-articles',
+        component: MyArticles,
+        beforeEnter: (to, from, next) => {
+            if (!store.getters['auth/authenticated']) {
+                return next({
+                    name: 'main'
+                })
+            }
+            next()
+        }
+    },
+    {
+        path: '/update-article/:id',
+        component: UpdateArticle,
+        beforeEnter: (to, from, next) => {
+            if (!store.getters['auth/authenticated']) {
+                return next({
+                    name: 'main'
+                })
+            }
+            next()
+        }
     }
 ]
 
 const router = createRouter({
-    routes,
-    history: createWebHistory(process.env.BASE_URL)
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router;
--- src/store/auth.js
+++ src/store/auth.js
@@ -8,6 +8,16 @@ export default ({
         user: null
     },
 
+    getters: {
+        authenticated(state) {
+            return state.token && state.user
+        },
+
+        user(state) {
+            return state.user
+        }
+    },
+
     mutations: {
         SET_TOKEN(state, token) {
             state.token = token
@@ -19,21 +29,31 @@ export default ({
     },
 
     actions: {
-        async login({dispatch}, credentials) {
+        async login({ dispatch }, credentials) {
             const response = await axios.post(`http://localhost:8080/api/login`, credentials);
-            dispatch('attempt', response.data)
+            return dispatch('attempt', response.data.token)
         },
 
-        async attempt({commit}, token) {
-            commit('SET_TOKEN', token)
+        async attempt({ commit, state }, token) {
+            if (token) {
+                commit('SET_TOKEN', token)
+            }
+
+            if (!state.token) {
+                return
+            }
 
             try {
-                const response = await axios.post(`http://localhost:8080/api/session`, token)
+                const response = await axios.get(`http://localhost:8080/api/session`)
                 commit('SET_USER', response.data.login)
             } catch (e) {
                 commit('SET_TOKEN', null)
                 commit('SET_USER', null)
             }
+        },
+        logOut({ commit }) {
+            commit('SET_TOKEN', null)
+            commit('SET_USER', null)
         }
     }
 })
